0339a4e16aba5761bcd59fd489cd9514a26a361a
MYNT Network <74799942+marnion@users.noreply.github.com>
2020-12-27 22:13:55 -0500

    Update elite-config.js

--- assets/jsa/elite-config.js
+++ assets/jsa/elite-config.js
@@ -1,5 +1,5 @@
 var metacoinConfig = {
-  "contractAddress": "TP44jH8RoyinS35SV951a9Atd6h6qngP2g",
+  "contractAddress": "TXp6LPTmrM35Me8objs5PV5WQYiQe3jxQQ",
   "privateKey": "da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0",
   "fullHost": "https://api.tronstack.io"
 }
